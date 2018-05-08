bbad49842d66afb4031d19c8dd6bde9c171a40b4
Daniel Facciolo Pires <Daniel.Pires@fatec.local>
2018-05-08 10:55:11 -0300

    com Vetor de Objetos

--- VetorObjetosExe2.js
+++ VetorObjetosExe2.js
@@ -2,40 +2,47 @@ function cadastra(){
 	// cria um objeto
 	var objeto = new Object();
 	// atribui valores às propriedades
-	objeto.nome = prompt("Informe nome");
-	objeto.p1 = parseFloat(prompt("Informe p1"));
-	objeto.p2 = parseFloat(prompt("Informe p2"));
-	objeto.media = (objeto.p1 + objeto.p2) / 2;
+	objeto.salario = parseFloat(prompt("Informe salário"));
+	objeto.filhos = parseInt(prompt("Informe filhos"));
+	objeto.idade = parseInt(prompt("Informe idade"));
+	objeto.sexo = prompt("Informe sexo: f para feminio e m para masculino");
 	// adiciono o objeto no vetor	
 	vetor.push(objeto);
-	alert("Aluno cadastrado com sucesso")
+	alert("Habitante cadastrado com sucesso");
 }
-function lista(){
-	var saida = "";
+function mediaSalario(){
+	var media=0;
 	for(var i=0;i<vetor.length;i++){
-		saida = saida + (vetor[i].nome + " " + vetor[i].media);
-		if (vetor[i].media >=6){
-			saida = saida + " - Aprovado";
-		}
-		else if (vetor[i].media >=3){
-			saida = saida + " - Exame";
-		} 
-		else saida = saida + " - Reprovado";
-		saida = saida + "<br/>"
+		media = media + vetor[i].salario;
 	}
-	document.getElementById("lista").innerHTML = saida;
+	media = media / vetor.length;
+	alert("A média de salários é " + media);
 }
-function limpa(){
-	vetor = [];
+function mediaFilhos(){
+	var media=0;
+	for(var i=0;i<vetor.length;i++){
+		media = media + vetor[i].filhos;
+	}
+	media = media / vetor.length;
+	alert("A média de filhos é " + media);
 }
-function maiorMedia(){
-	var maior = vetor[0].media; // assumo a maior média
-	var nome = vetor[0].nome;
+function maiorSalario(){
+	var maior = vetor[0].salario; // assumo a maior média
 	for(var i=1;i<vetor.length;i++){
-		if (vetor[i].media > maior){
-			maior = vetor[i].media;
-			nome = vetor[i].nome;
+		if (vetor[i].salario > maior){
+			maior = vetor[i].salario;
 		}
 	}
-	alert("Nome do aluno com maior média " + nome);
+	alert("Maior salário " + maior);
 }
+function percentual(){
+	var conta=0;
+	for(var i=0;i<vetor.length;i++){
+		if ((vetor[i].salario >= 1000) && (vetor[i].sexo == "f")){
+			conta++;
+		}
+	}
+	
+	alert("Percentual " + conta/vetor.length*100 + "%");
+}
+
