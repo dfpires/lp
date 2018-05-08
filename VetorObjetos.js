2c57ba2ff8977889fec00d00a430b97668a3b917
Daniel Facciolo Pires <Daniel.Pires@fatec.local>
2018-05-08 10:01:32 -0300

    com Vetor de Objetos

--- VetorObjetos.js
+++ VetorObjetos.js
@@ -13,7 +13,29 @@ function cadastra(){
 function lista(){
 	var saida = "";
 	for(var i=0;i<vetor.length;i++){
-		saida = saida + (vetor[i].nome + " " + vetor[i].media) + "<br/>";
+		saida = saida + (vetor[i].nome + " " + vetor[i].media);
+		if (vetor[i].media >=6){
+			saida = saida + " - Aprovado";
+		}
+		else if (vetor[i].media >=3){
+			saida = saida + " - Exame";
+		} 
+		else saida = saida + " - Reprovado";
+		saida = saida + "<br/>"
 	}
 	document.getElementById("lista").innerHTML = saida;
+}
+function limpa(){
+	vetor = [];
+}
+function maiorMedia(){
+	var maior = vetor[0].media; // assumo a maior média
+	var nome = vetor[0].nome;
+	for(var i=1;i<vetor.length;i++){
+		if (vetor[i].media > maior){
+			maior = vetor[i].media;
+			nome = vetor[i].nome;
+		}
+	}
+	alert("Nome do aluno com maior média " + nome);
 }
