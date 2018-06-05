b542de3936dc47d6d6f893950baf7265fada59f4
Daniel Facciolo Pires <Daniel.Pires@fatec.local>
2018-06-05 09:04:28 -0300

    exe1 funcoes

--- Lista de Vetor de Objetos/funcoes.js
+++ Lista de Vetor de Objetos/funcoes.js
@@ -1,4 +1,24 @@
-function exe1(n){
+function principal(){
+	// exercício 1
+	var n = parseInt(prompt("Informe um valor"));
+	alert("Soma dos elementos " + exe1(n));
+	// exercício 2
 	
-	return 
+}
+function exe2(ph, pm, ps){
+	
+}
+
+// retorna -1 caso o valor de n seja negativo ou 0
+function exe1(pn){
+	if (pn <= 0){
+		return -1; // retorna
+	}
+	else {
+		var conta = 0;
+		for(var i =1;i<=pn;i++){
+			conta = conta + i;
+		}
+		return conta; // retorna
+	}
 }
